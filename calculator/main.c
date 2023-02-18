35ea673e4afd5310baca4881071eadafb7a994b0
Kwiat0stan <122578717+Kwiat0stan@users.noreply.github.com>
2023-02-18 22:48:56 +0000

    modified:   calculator/main 	modified:   calculator/main.c

--- calculator/main.c
+++ calculator/main.c
@@ -2,8 +2,30 @@
 
 int main()
 {
-    int num;
+    int num, num2;
+    char operator;
     printf("Enter the first number: ");
-    scanf("");
+    scanf("%d", &num);
+    printf("Enter operator: ");
+    scanf(" %c", &operator);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    switch(operator)
+    {
+        case '+':
+            printf("Twoja liczba to: %d.", num + num2);
+            break;
+        case '-':
+            printf("Twoja liczba to: %d.", num - num2);
+            break;
+        case '*':
+            printf("Twoja liczba to: %d.", num * num2);
+            break;
+        case '/':
+            printf("Twoja liczba to: %d.", num / num2);
+        default:
+            break;
+    }
+    printf("\n");
     return 0;
 }
